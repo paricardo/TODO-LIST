adb68c4ff33c4d4a997e3bae830e824494fd4056
Paulo <prbastosdev@gmail.com>
2026-05-24 16:04:35 -0300

    jwt-study-initiated

--- server.ts
+++ server.ts
@@ -1,5 +1,26 @@
 import express from 'express';
 import router from './routes.js';
+import jwt from 'jsonwebtoken';
+
+const payload = {
+    userId: '9ZVBauiKLR9tTB',
+    name: "Paulo Ricardo",
+}
+
+const secret = 'GjRzvkj3TNEkfw';
+
+const option: jwt.SignOptions = {
+    expiresIn: '1h'
+}
+
+const token = jwt.sign(payload, secret, option);
+
+try {
+    const decoded = jwt.verify(token, 'teste');
+    console.log(decoded);
+} catch (error: any) {   
+    console.log({error: error.message});
+}
 
 const server = express();
 
